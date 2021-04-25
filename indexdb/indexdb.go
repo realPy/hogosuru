79a9959293fb4140b63fec57570d00747f145c98
Manuel Barraud <manuel.barraud@biig.fr>
2021-04-25 19:13:53 +0200

    Create Schema and store data on indexdb

--- indexdb/indexdb.go
+++ indexdb/indexdb.go
@@ -3,7 +3,6 @@ package indexdb
 import (
 	"fmt"
 
-	"github.com/realPy/jswasm"
 	"github.com/realPy/jswasm/js"
 )
 
@@ -11,18 +10,55 @@ type IndexDB struct {
 	dbObject js.Value
 }
 
-func OpenIndexDB(name string) (IndexDB, error) {
+type SuccessFailure struct {
+	Success bool
+	Payload []js.Value
+}
+
+func OnSuccessFailure(awaitable js.Value) chan SuccessFailure {
+	ch := make(chan SuccessFailure)
+	cbok := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		ch <- SuccessFailure{Success: true, Payload: args}
+		return nil
+	})
+	cberror := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		ch <- SuccessFailure{Success: false, Payload: args}
+		return nil
+	})
+	awaitable.Set("onsuccess", cbok)
+	awaitable.Set("onerror", cberror)
+	return ch
+}
+
+func OpenIndexDB(name string, automigrate func(js.Value) error) (IndexDB, error) {
 	var indexdb IndexDB
 
 	if window, err := js.Global().GetWithErr("window"); err == nil {
 
 		if indexedDB, err := window.GetWithErr("indexedDB"); err == nil {
 
-			if waitableOpen, err := indexedDB.CallWithErr("open", js.ValueOf("test")); err == nil {
-				ch := jswasm.OnSuccessFailure(waitableOpen)
+			if waitableOpen, err := indexedDB.CallWithErr("open", js.ValueOf(name)); err == nil {
+				test := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+
+					db := args[0].Get("target").Get("result")
+					return automigrate(db)
+
+				})
+				waitableOpen.Set("onupgradeneeded", test)
+				ch := OnSuccessFailure(waitableOpen)
 				results := <-ch
 				if results.Success {
-					indexdb.dbObject = results.Payload[0]
+					ev := results.Payload[0]
+					if target, err := ev.GetWithErr("target"); err == nil {
+						if db, err := target.GetWithErr("result"); err == nil {
+							indexdb.dbObject = db
+						} else {
+							return IndexDB{}, fmt.Errorf("result not found")
+						}
+					} else {
+						return IndexDB{}, fmt.Errorf("target not found")
+					}
+
 					return indexdb, nil
 				} else {
 					return IndexDB{}, fmt.Errorf("Open IndexDB failed")
@@ -39,4 +75,19 @@ func OpenIndexDB(name string) (IndexDB, error) {
 		return IndexDB{}, err
 	}
 
+}
+
+func (i IndexDB) Store(table string, value map[string]interface{}) error {
+
+	if transaction, err := i.dbObject.CallWithErr("transaction", js.ValueOf(table), js.ValueOf("readwrite")); err == nil {
+
+		if objectstore, err := transaction.CallWithErr("objectStore", js.ValueOf(table)); err == nil {
+			objectstore.CallWithErr("add", js.ValueOf(value))
+			return nil
+		} else {
+			return err
+		}
+	} else {
+		return err
+	}
 }
---
+++ indexdb/store.go
@@ -0,0 +1,24 @@
+package indexdb
+
+import (
+	"github.com/realPy/jswasm/js"
+)
+
+type Store struct {
+	storeObject js.Value
+}
+
+func CreateStore(dbObject js.Value, name string, schema map[string]interface{}) (Store, error) {
+	var store Store
+	if storeObject, err := dbObject.CallWithErr("createObjectStore", js.ValueOf(name), schema); err == nil {
+		store.storeObject = storeObject
+		return store, nil
+	} else {
+		return Store{}, err
+	}
+}
+
+func (s Store) CreateIndex(nameIndex string, nameKey string, option map[string]interface{}) error {
+	_, err := s.storeObject.CallWithErr("createIndex", js.ValueOf(nameIndex), js.ValueOf(nameKey), js.ValueOf(option))
+	return err
+}
