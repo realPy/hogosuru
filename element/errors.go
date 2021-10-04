0ba190fb645fa9cb06a55a66da297113707083d6
RealPy <t3sla@v-ip.fr>
2021-10-04 18:59:00 +0200

    check if getelements return null

--- element/errors.go
+++ element/errors.go
@@ -4,9 +4,11 @@ import "errors"
 
 var (
 	//ErrNotImplemented ErrNotImplemented error
-	ErrNotImplemented  = errors.New("Browser not implemented Node")
-	ErrNotAnElement    = errors.New("Object is not an Element")
-	ErrElementNoChilds = errors.New("Element has no childs")
-	ErrAttributeEmpty  = errors.New("Attribute is empty")
-	ErrInsertAdajacent = errors.New("Insert Adjacent failed")
+	ErrNotImplemented   = errors.New("Browser not implemented Node")
+	ErrNotAnElement     = errors.New("Object is not an Element")
+	ErrElementNoChilds  = errors.New("Element has no childs")
+	ErrAttributeEmpty   = errors.New("Attribute is empty")
+	ErrInsertAdajacent  = errors.New("Insert Adjacent failed")
+	ErrElementNotFound  = errors.New("Element not Found")
+	ErrElementsNotFound = errors.New("Elements not Found")
 )
