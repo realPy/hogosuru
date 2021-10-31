093e8b3f5b6475a42a853ef0fe795b09dce34204
RealPy <t3sla@v-ip.fr>
2021-10-31 10:53:07 +0100

    Add keyboard event

---
+++ keyboardevent/keyboardevent.go
@@ -0,0 +1,137 @@
+package keyboardevent
+
+// https://developer.mozilla.org/fr/docs/Web/API/CustomEvent
+import (
+	"sync"
+
+	"syscall/js"
+
+	"github.com/realPy/hogosuru/baseobject"
+	"github.com/realPy/hogosuru/event"
+)
+
+var singleton sync.Once
+
+var customeventinterface js.Value
+
+//KeyboardEvent KeyboardEvent struct
+type KeyboardEvent struct {
+	event.Event
+}
+
+type KeyboardEventFrom interface {
+	KeyboardEvent_() KeyboardEvent
+}
+
+func (k KeyboardEvent) KeyboardEvent_() KeyboardEvent {
+	return k
+}
+
+//GetInterface get the KeyboardEvent interface
+func GetInterface() js.Value {
+
+	singleton.Do(func() {
+		var err error
+		if customeventinterface, err = baseobject.Get(js.Global(), "KeyboardEvent"); err != nil {
+			customeventinterface = js.Undefined()
+		}
+
+		baseobject.Register(customeventinterface, func(v js.Value) (interface{}, error) {
+			return NewFromJSObject(v)
+		})
+
+	})
+
+	return customeventinterface
+}
+
+//New Create a KeyboardEvent
+func New(typeK string, opt ...map[string]interface{}) (KeyboardEvent, error) {
+	var event KeyboardEvent
+	var obj js.Value
+	var err error
+	var arrayJS []interface{}
+
+	if eventi := GetInterface(); !eventi.IsUndefined() {
+
+		arrayJS = append(arrayJS, js.ValueOf(typeK))
+		if len(opt) > 0 {
+			arrayJS = append(arrayJS, js.ValueOf(opt[0]))
+		}
+
+		if obj, err = baseobject.New(eventi, arrayJS...); err == nil {
+			event.BaseObject = event.SetObject(obj)
+		}
+
+	} else {
+		err = ErrNotImplemented
+	}
+	return event, err
+}
+
+func NewFromJSObject(obj js.Value) (KeyboardEvent, error) {
+	var k KeyboardEvent
+	var err error
+
+	if bi := GetInterface(); !bi.IsUndefined() {
+		if obj.IsUndefined() {
+			err = baseobject.ErrUndefinedValue
+		} else {
+
+			if obj.InstanceOf(bi) {
+				k.BaseObject = k.SetObject(obj)
+
+			} else {
+				err = ErrNotAKeyboardEvent
+			}
+		}
+	} else {
+		err = ErrNotImplemented
+	}
+
+	return k, err
+}
+
+func (k KeyboardEvent) AltKey() (bool, error) {
+
+	return k.GetAttributeBool("altKey")
+}
+
+func (k KeyboardEvent) CtrlKey() (bool, error) {
+
+	return k.GetAttributeBool("ctrlKey")
+}
+func (k KeyboardEvent) Key() (string, error) {
+
+	return k.GetAttributeString("key")
+}
+
+func (k KeyboardEvent) Code() (string, error) {
+
+	return k.GetAttributeString("code")
+}
+
+func (k KeyboardEvent) IsComposing() (bool, error) {
+
+	return k.GetAttributeBool("isComposing")
+}
+
+func (k KeyboardEvent) Location() (int64, error) {
+
+	return k.GetAttributeInt64("location")
+}
+
+func (k KeyboardEvent) MetaKey() (bool, error) {
+
+	return k.GetAttributeBool("metaKey")
+}
+
+func (k KeyboardEvent) Repeat() (bool, error) {
+
+	return k.GetAttributeBool("repeat")
+}
+
+func (k KeyboardEvent) ShiftKey() (bool, error) {
+
+	return k.GetAttributeBool("shiftKey")
+}
---
+++ keyboardevent/keyboardevent_test.go
@@ -0,0 +1,184 @@
+package keyboardevent
+
+import (
+	"syscall/js"
+	"testing"
+
+	"github.com/realPy/hogosuru/baseobject"
+	"github.com/realPy/hogosuru/testingutils"
+)
+
+func TestMain(m *testing.M) {
+	baseobject.SetSyscall()
+
+	m.Run()
+}
+
+func TestNew(t *testing.T) {
+
+	if k, err := New("keypress", map[string]interface{}{"code": "KeyA"}); testingutils.AssertErr(t, err) {
+
+		testingutils.AssertExpect(t, "[object KeyboardEvent]", k.ToString_())
+		if v, err := k.Code(); testingutils.AssertErr(t, err) {
+			testingutils.AssertExpect(t, "KeyA", v)
+		}
+	}
+}
+
+func TestNewFromJSObject(t *testing.T) {
+
+	baseobject.Eval("keypress=new KeyboardEvent(\"keyup\")")
+
+	if obj, err := baseobject.Get(js.Global(), "keypress"); testingutils.AssertErr(t, err) {
+		if k, err := NewFromJSObject(obj); testingutils.AssertErr(t, err) {
+
+			testingutils.AssertExpect(t, "[object KeyboardEvent]", k.ToString_())
+
+		}
+	}
+
+}
+
+func TestAltKey(t *testing.T) {
+
+	baseobject.Eval("keypress=new KeyboardEvent(\"keyup\",{altKey:true})")
+
+	if obj, err := baseobject.Get(js.Global(), "keypress"); testingutils.AssertErr(t, err) {
+		if k, err := NewFromJSObject(obj); testingutils.AssertErr(t, err) {
+
+			if v, err := k.AltKey(); testingutils.AssertErr(t, err) {
+				testingutils.AssertExpect(t, true, v)
+			}
+
+		}
+	}
+
+}
+
+func TestCtrlKey(t *testing.T) {
+
+	baseobject.Eval("keypress=new KeyboardEvent(\"keyup\",{ctrlKey:true})")
+
+	if obj, err := baseobject.Get(js.Global(), "keypress"); testingutils.AssertErr(t, err) {
+		if k, err := NewFromJSObject(obj); testingutils.AssertErr(t, err) {
+
+			if v, err := k.CtrlKey(); testingutils.AssertErr(t, err) {
+				testingutils.AssertExpect(t, true, v)
+			}
+
+		}
+	}
+
+}
+
+func TestKey(t *testing.T) {
+
+	baseobject.Eval("keypress=new KeyboardEvent(\"keyup\",{key:\"Enter\"})")
+
+	if obj, err := baseobject.Get(js.Global(), "keypress"); testingutils.AssertErr(t, err) {
+		if k, err := NewFromJSObject(obj); testingutils.AssertErr(t, err) {
+
+			if v, err := k.Key(); testingutils.AssertErr(t, err) {
+				testingutils.AssertExpect(t, "Enter", v)
+			}
+
+		}
+	}
+
+}
+
+func TestCode(t *testing.T) {
+
+	baseobject.Eval("keypress=new KeyboardEvent(\"keyup\",{code:\"KeyA\"})")
+
+	if obj, err := baseobject.Get(js.Global(), "keypress"); testingutils.AssertErr(t, err) {
+		if k, err := NewFromJSObject(obj); testingutils.AssertErr(t, err) {
+
+			if v, err := k.Code(); testingutils.AssertErr(t, err) {
+				testingutils.AssertExpect(t, "KeyA", v)
+			}
+
+		}
+	}
+
+}
+
+func TestIsComposing(t *testing.T) {
+
+	baseobject.Eval("keypress=new KeyboardEvent(\"keyup\",{isComposing:true})")
+
+	if obj, err := baseobject.Get(js.Global(), "keypress"); testingutils.AssertErr(t, err) {
+		if k, err := NewFromJSObject(obj); testingutils.AssertErr(t, err) {
+
+			if v, err := k.IsComposing(); testingutils.AssertErr(t, err) {
+				testingutils.AssertExpect(t, true, v)
+			}
+
+		}
+	}
+
+}
+
+func TestLocation(t *testing.T) {
+
+	baseobject.Eval("keypress=new KeyboardEvent(\"keyup\",{location:1})")
+
+	if obj, err := baseobject.Get(js.Global(), "keypress"); testingutils.AssertErr(t, err) {
+		if k, err := NewFromJSObject(obj); testingutils.AssertErr(t, err) {
+
+			if v, err := k.Location(); testingutils.AssertErr(t, err) {
+				testingutils.AssertExpect(t, int64(1), v)
+			}
+
+		}
+	}
+
+}
+
+func TestMetaKey(t *testing.T) {
+
+	baseobject.Eval("keypress=new KeyboardEvent(\"keyup\",{metaKey:true})")
+
+	if obj, err := baseobject.Get(js.Global(), "keypress"); testingutils.AssertErr(t, err) {
+		if k, err := NewFromJSObject(obj); testingutils.AssertErr(t, err) {
+
+			if v, err := k.MetaKey(); testingutils.AssertErr(t, err) {
+				testingutils.AssertExpect(t, true, v)
+			}
+
+		}
+	}
+
+}
+
+func TestRepeat(t *testing.T) {
+
+	baseobject.Eval("keypress=new KeyboardEvent(\"keyup\",{repeat:true})")
+
+	if obj, err := baseobject.Get(js.Global(), "keypress"); testingutils.AssertErr(t, err) {
+		if k, err := NewFromJSObject(obj); testingutils.AssertErr(t, err) {
+
+			if v, err := k.Repeat(); testingutils.AssertErr(t, err) {
+				testingutils.AssertExpect(t, true, v)
+			}
+
+		}
+	}
+
+}
+
+func TestShiftKey(t *testing.T) {
+
+	baseobject.Eval("keypress=new KeyboardEvent(\"keyup\",{shiftKey:true})")
+
+	if obj, err := baseobject.Get(js.Global(), "keypress"); testingutils.AssertErr(t, err) {
+		if k, err := NewFromJSObject(obj); testingutils.AssertErr(t, err) {
+
+			if v, err := k.ShiftKey(); testingutils.AssertErr(t, err) {
+				testingutils.AssertExpect(t, true, v)
+			}
+
+		}
+	}
+
+}
