96e93a500118adefa460c15a0d4cf3546f002cb4
RealPy <t3sla@v-ip.fr>
2021-11-03 18:10:17 +0100

    ad test unit for htmlembed

---
+++ htmlembedelement/htmlembedelement_test.go
@@ -0,0 +1,86 @@
+package htmlembedelement
+
+import (
+	"syscall/js"
+	"testing"
+
+	"github.com/realPy/hogosuru/baseobject"
+	"github.com/realPy/hogosuru/document"
+	"github.com/realPy/hogosuru/testingutils"
+)
+
+func TestMain(m *testing.M) {
+	baseobject.SetSyscall()
+	baseobject.Eval(`e= document.createElement("embed")
+	`)
+	m.Run()
+}
+
+func TestNew(t *testing.T) {
+
+	if doc, err := document.New(); testingutils.AssertErr(t, err) {
+		if b, err := New(doc); testingutils.AssertErr(t, err) {
+			testingutils.AssertExpect(t, "HTMLEmbedElement", b.ConstructName_())
+		}
+
+	}
+}
+
+func TestNewFromJSObject(t *testing.T) {
+
+	if obj, err := baseobject.Get(js.Global(), "e"); testingutils.AssertErr(t, err) {
+
+		if b, err := NewFromJSObject(obj); testingutils.AssertErr(t, err) {
+
+			testingutils.AssertExpect(t, "HTMLEmbedElement", b.ConstructName_())
+		}
+
+	}
+}
+
+var getterAttempt []map[string]interface{} = []map[string]interface{}{
+	{"method": "Height", "resultattempt": ""},
+	{"method": "Src", "resultattempt": ""},
+	{"method": "Type", "resultattempt": ""},
+	{"method": "Width", "resultattempt": ""},
+}
+
+func TestGetters(t *testing.T) {
+
+	if obj, err := baseobject.Get(js.Global(), "e"); testingutils.AssertErr(t, err) {
+
+		if button, err := NewFromJSObject(obj); testingutils.AssertErr(t, err) {
+
+			for _, result := range getterAttempt {
+				testingutils.InvokeCheck(t, button, result)
+			}
+
+		}
+
+	}
+}
+
+var setterAttempt []map[string]interface{} = []map[string]interface{}{
+	{"method": "SetHeight", "args": []interface{}{"value"}, "gettermethod": "Height", "resultattempt": "value"},
+	{"method": "SetSrc", "args": []interface{}{"value"}, "gettermethod": "Src", "type": "contains", "resultattempt": "/value"},
+	{"method": "SetType", "args": []interface{}{"value"}, "gettermethod": "Type", "resultattempt": "value"},
+	{"method": "SetWidth", "args": []interface{}{"value"}, "gettermethod": "Width", "resultattempt": "value"},
+}
+
+func TestSetters(t *testing.T) {
+
+	if obj, err := baseobject.Get(js.Global(), "e"); testingutils.AssertErr(t, err) {
+
+		if button, err := NewFromJSObject(obj); testingutils.AssertErr(t, err) {
+
+			for _, result := range setterAttempt {
+
+				testingutils.InvokeCheck(t, button, result)
+
+			}
+
+		}
+
+	}
+
+}
